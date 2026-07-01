02222f3c762c96b65c61da3ffefafe1d8a8363b9
tterreiro <hde-andr@student.42porto.com>
2026-07-01 17:05:54 +0100

    adaw

--- src/map_parsing.py
+++ src/map_parsing.py
@@ -22,13 +22,20 @@ class MapParser:
         for token in tokens:
             if '=' in token:
                 key, value = token.split('=', 1)
-                if key == 'zone' and value not in ZoneType:
+                key = key.lower()
+                if key == 'zone' and value.lower() not in ZoneType:
                     raise ParsingError("Invalid zone type. Must be Normal, "
                                        "Blocked, Restricted or Priority")
-                elif key == 'color' and value not in ZoneType:
-                    raise ParsingError("Invalid zone type. Must be Normal, "
-                                       "Blocked, Restricted or Priority")
-                metadata[key.strip()] = value.strip()
+                elif key == 'color' and not value.isalpha():
+                    raise ParsingError("Invalid colour value! "
+                                       "Must be a single-word string")
+                elif key == 'max_drones' and int(value) <= 0:
+                    raise ParsingError("Invalid ammount of drones."
+                                       "Must be more than 0.")
+                elif key == 'max_link_capacity' and int(value) <= 0:
+                    raise ParsingError("Invalid ammount of max link capacity"
+                                       "drones. Must be more than 0.")
+                metadata[key.strip()] = value.lower().strip()
         return metadata
 
     @staticmethod
@@ -37,7 +44,10 @@ class MapParser:
         graph = Graph()
         raw_zones: list[str] = []
         raw_connections: list[str] = []
+        zones_names = []
         is_first_line = True
+        nb_start_hub = 0
+        nb_end_hub = 0
         with open(input_file, "r") as f:
             for line_num, line in enumerate(f, 1):
                 clean_line = line.split('#')[0].strip().lower()
@@ -75,6 +85,21 @@ class MapParser:
         for zone in raw_zones:
             if '[' in zone:
                 metadata = MapParser.parse_metadata(zone.split('[', 1)[1])
-                zone = zone.split('[', 1)[1]
+                zone = zone.split('[', 1)[0].strip()
             if zone.startswith("start_hub"):
-                pass
+                data: list[str] = zone.split(' ')
+                if len(data) > 4:
+                    raise ParsingError("Hub creation must follow the rules: "
+                                       "start_hub: <name> <x> <y>")
+                name = data[1]
+                (x, y) = (data[2], data[3])
+                if ' ' in name or '-' in name:
+                    raise ParsingError("Zone names mustn't contain spaces or "
+                                       "dashes!")
+                if name in zones_names:
+                    raise ParsingError("Every the zone name must be unique!")
+                if not int(x) or not int(y):
+                    raise ParsingError("Zones coordinates must be numbers!"
+                                       f" ({x}, {y})")
+                zones_names.append(name)
+                nb_start_hub += 1
